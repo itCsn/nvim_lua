f5d54ef1bb12bede95009a73a39dc243da356461
itCsn <82104936+itCsn@users.noreply.github.com>
2021-09-17 12:10:24 -0500

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,24 +13,36 @@ return require('packer').startup(
     -- Info
     use 'nanotee/nvim-lua-guide'
 
-	-- Utils
-	use 'hrsh7th/vim-vsnip' -- Snips
-	use 'windwp/nvim-autopairs'	 --autopairs
-	--use 'frazrepo/vim-rainbow' --color for brackets
+    -- Utils
+    use 'hrsh7th/vim-vsnip' -- Snips
+    use 'windwp/nvim-autopairs'	 --autopairs
+    --use 'frazrepo/vim-rainbow' --color for brackets
+    use 'hrsh7th/vim-vsnip-integ'
+    use 'p00f/nvim-ts-rainbow'
+    use 'mbbill/undotree'
+    use 'ryanoasis/vim-devicons'
+    use 'onsails/lspkind-nvim'
+    use 'sheerun/vim-polyglot'
+    use 'tpope/vim-commentary'
+    use 'frazrepo/vim-rainbow'
 
 
-	-- Icons
-	use 'kyazdani42/nvim-web-devicons'
 
-	-- Theme
-	use 'gruvbox-community/gruvbox'
-	use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+    -- Icons
+    use 'kyazdani42/nvim-web-devicons'
+		
+    -- Theme
+    use 'gruvbox-community/gruvbox'
+    use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+	
+    -- treesitter
+    use 'nvim-treesitter/nvim-treesitter'
 
-	-- treesitter
-	use 'nvim-treesitter/nvim-treesitter'
-
-	-- file manager
-
+    -- file manager
+    use {
+	'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons'
+}
+		
 end)
 
 
